63a2f11240ba410d614a19144583f5f9043db5b6
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-09-19 14:11:29 -0400

    .

--- main.py
+++ main.py
@@ -6,11 +6,40 @@ import json
 app = Flask(__name__)
 books = [{'id': 1, 'title': 'Python Essentials', 'author': 'Jane Doe'}]
 
-ongoing = {}
+
+class Tracker:
+    def __init__(self) -> None:
+        self.tracker = {}
+        super.__init__()
+    
+    def is_zone_occupied(self, zone):
+        first, second = self.tracker.get(zone, (None, None))
+        return first != None
+    
+    def set_zone_empty(self, zone):
+        self.tracker[zone] = (None, None)
+    
+    def set_zone_occupied(self, zone, first_event):
+        self.tracker[zone] = (first_event, None)
+    
+    def update_zone_last_event(self, zone, last_event):
+        self.tracker[zone] = (self.tracker[zone][0], last_event)
+
+
+tracker = Tracker()
+
 
 timestr = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 file = open(f'{timestr}.txt', 'w')
+
+def print_status():
+    print("Zone statuses:")
+    for zone in tracker.tracker:
+        print(f'{zone}: {tracker.is_zone_occupied(zone)}')
+
+def ok():
+    return {}, 200
 
 @app.route('/', methods=['GET'])
 def get_books():
@@ -19,25 +48,18 @@ def get_books():
 @app.route('/', methods=['POST'])
 def post_data():
     data = request.get_json()
-    if data == None or data["Events"] == None:
-        return jsonify(isError= False,
-                    message= "Success",
-                    statusCode= 200,
-                    data = data), 200
+    if data == None or "Events" not in data or "event" not in data["Events"]:
+        print("data or events or events-event was none!")
+        return ok()
     
-    file.write(json.dumps(data, indent=4))
-
-    # for event in data["Events"]["event"]:
-    #     if (event["status"] != "new"):
-    #         pass #continue
-    #     #event["frame"]["attribute"] = []
-    #     event["attribute"] = []
-
+    if data["Events"]["event"]["@type"] != "alarm":
+        print("event wasnt an alarm")
+        return ok()
     
-    return jsonify(isError= False,
-                    message= "Success",
-                    statusCode= 200,
-                    data= data), 200
+    zone = data["Events"]["event"]["spy-name"]
+    print(zone + " event")
+        
+    return {}, 200
 
 if __name__ == '__main__':
     logging.basicConfig(filename='error.log',level=logging.DEBUG)
