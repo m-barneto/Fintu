badaa93c1682c848d0ff2c895a28782edb240346
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-09-19 15:49:05 -0400

    .

--- main.py
+++ main.py
@@ -69,6 +69,7 @@ def get_books():
 def post_data():
 
     data = request.get_json(silent=True)
+    print(data)
     if data == None or "Events" not in data or "event" not in data["Events"]:
         # print("data or events or events-event was none!")
         return ok()
