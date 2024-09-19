17f95ad5dae9b6760dd2d2e9b9d36eec034c2d23
m-barneto <4347791+m-barneto@users.noreply.github.com>
2024-09-19 09:24:55 -0400

    initial

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+books = [{'id': 1, 'title': 'Python Essentials', 'author': 'Jane Doe'}]
+
+@app.route('/', methods=['GET'])
+def get_books():
+    return jsonify({'books': books})
+
+@app.route('/', methods=['POST'])
+def post_data():
+    data = request.form
+    print(data)
+    return jsonify(isError= False,
+                    message= "Success",
+                    statusCode= 200,
+                    data= data), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
